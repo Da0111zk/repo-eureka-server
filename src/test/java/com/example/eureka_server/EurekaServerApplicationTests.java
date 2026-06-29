3291329d151a31ef4f8747cbf1ec579084db866e
Da0111zk <da.azocard@duocuc.cl>
2026-06-28 22:18:14 -0400

    feat: eureka-server independiente

---
+++ src/test/java/com/example/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bodega.eurekaserver.EurekaServerApplication;
+
+@SpringBootTest(classes = EurekaServerApplication.class) 
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
